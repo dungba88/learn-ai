6bcebd77cf67d094bbcd7360506e5da38bfe1494
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-11-04 19:22:05 +0800

    fix bug

--- minibatch/generator.py
+++ minibatch/generator.py
@@ -5,39 +5,45 @@ class MiniBatchGenerator(object):
 
     def __init__(self, max_size: int, x_mapper: callable, y_mapper: callable):
         self.arr = [idx for idx in range(max_size)]
+        self.max_size = max_size
         self.x_mapper = x_mapper
         self.y_mapper = y_mapper
-        self.train_idx = 0
-        self.train_size = max_size
-        self.test_idx = max_size
         random.shuffle(self.arr)
+        self.train_idx = 0
+        self.test_idx = 0
+        self.train_arr = self.arr[:]
+        self.test_arr = []
+        self.reset()
 
     def split_train_test(self, test_size=0.2):
-        self.test_idx = int(len(self.arr) * (1 - test_size))
-        self.train_size = self.test_idx
+        train_size = int(self.max_size * (1 - test_size))
+        self.train_arr = self.arr[:train_size]
+        self.test_arr = self.arr[train_size:]
+        self.reset()
 
     def load_next_train_batch(self, batch_size: int):
-        batch = self.__next_batch(batch_size, self.train_idx, self.train_size)
+        batch = self.__next_batch(batch_size, self.train_idx, self.train_arr)
         self.train_idx += len(batch)
         return self.__load(batch)
 
     def load_next_test_batch(self, batch_size: int):
-        batch = self.__next_batch(batch_size, self.test_idx, len(self.arr))
+        batch = self.__next_batch(batch_size, self.test_idx, self.test_arr)
         self.test_idx += len(batch)
         return self.__load(batch)
 
     def reset(self):
         self.train_idx = 0
-        self.test_idx = self.train_size
-        random.shuffle(self.arr)
+        self.test_idx = 0
+        random.shuffle(self.train_arr)
+        random.shuffle(self.test_arr)
 
-    def __next_batch(self, batch_size, first_idx, last_idx):
-        remaining = last_idx - first_idx
+    def __next_batch(self, batch_size, first_idx, arr):
+        remaining = len(arr) - first_idx
         if remaining == 0:
             return []
         if batch_size > remaining:
             batch_size = remaining
-        result = self.arr[first_idx:first_idx + batch_size]
+        result = arr[first_idx:first_idx + batch_size]
         return result
 
     def __load(self, batch):
--- minibatch/main.py
+++ minibatch/main.py
@@ -1,22 +1,24 @@
 from generator import MiniBatchGenerator
 
 def main():
-    generator = MiniBatchGenerator(860, x_mapper, y_mapper)
+    generator = MiniBatchGenerator(10, x_mapper, y_mapper)
     generator.split_train_test()
 
-    print('load train mini-batch')
-    while True:
-        X, y = generator.load_next_train_batch(100)
-        if X is None:
-            break
-        print(X)
+    for i in range(10):
+        print('load train mini-batch')
+        generator.reset()
+        while True:
+            X, y = generator.load_next_train_batch(100)
+            if X is None:
+                break
+            print(X)
 
-    print('load test mini-batch')
-    while True:
-        X, y = generator.load_next_test_batch(100)
-        if X is None:
-            break
-        print(X)
+        print('load test mini-batch')
+        while True:
+            X, y = generator.load_next_test_batch(100)
+            if X is None:
+                break
+            print(X)
 
 def x_mapper(item):
     return "X" + str(item)
